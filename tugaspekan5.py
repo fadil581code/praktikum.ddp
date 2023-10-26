f20d6bc29dc01b077bd0fa3667768d3efe0fd400
fadil581code <146051075+fadil581code@users.noreply.github.com>
2023-10-26 10:36:20 +0700

    Add files via upload

---
+++ tugaspekan5.py
@@ -0,0 +1,9 @@
+motor= ["beat","sepeda motor","110","item","roda dua"]
+print(motor)
+print("=====================================")
+motor.append("5 juta")
+motor.append("cbs")
+print(motor)
+print("======================================")
+motor.insert(2,"yamaha")
+print(motor)
